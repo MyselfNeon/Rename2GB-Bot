c5f9bd722aa2494b93d37687047a20dae7f61fbd
NeonAn <justasneon@gmail.com>
2025-09-16 00:06:57 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -4,45 +4,21 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 from helper.database import jishubotz
 from config import Config, Txt  
 
-# Reaction list
-REACTIONS = [
-    "🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "🤩",
-    "😱", "🤣", "😘", "👏", "😛", "😈", "🎉", "⚡️", "🫡",
-    "🤓", "😎", "🏆", "🔥", "🤭", "🌚", "🆒", "👻", "😁"
-]
-# Don't add unsupported emojis because Telegram reactions have limits
-
 @Client.on_message(filters.private & filters.command("start"))
-async def start(client, message: Message):
-    user = message.from_user
-
-    # --- Reaction feature added here ---
-    try:
-        await message.react(emoji=random.choice(REACTIONS), big=True)
-    except Exception as e:
-        print(f"Reaction failed: {e}")
-    # -----------------------------------
-
-    await jishubotz.add_user(client, message)                
-    button = InlineKeyboardMarkup([
-        [InlineKeyboardButton('🔊 Uᴘᴅᴀᴛᴇs', url='https://t.me/NeonFiles'),
-         InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/+o1s-8MppL2syYTI9')],
-        [InlineKeyboardButton('❤️‍🩹 Aʙᴏᴜᴛ', callback_data='about'),
-         InlineKeyboardButton('🛠️ Hᴇʟᴘ', callback_data='help')],
-        [InlineKeyboardButton("👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/MyselfNeon')]
-    ])
-    if Config.START_PIC:
-        await message.reply_photo(
-            Config.START_PIC, 
-            caption=Txt.START_TXT.format(user.mention), 
-            reply_markup=button
-        )       
-    else:
-        await message.reply_text(
-            text=Txt.START_TXT.format(user.mention), 
-            reply_markup=button, 
-            disable_web_page_preview=True
-		)
+async def start(client, message):
+user = message.from_user
+await jishubotz.add_user(client, message)
+button = InlineKeyboardMarkup([
+[InlineKeyboardButton('🔊 Uᴘᴅᴀᴛᴇs', url='https://t.me/NeonFiles'),
+InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/+o1s-8MppL2syYTI9')],
+[InlineKeyboardButton('❤️‍🩹 Aʙᴏᴜᴛ', callback_data='about'),
+InlineKeyboardButton('🛠️ Hᴇʟᴘ', callback_data='help')],
+[InlineKeyboardButton("👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/MyselfNeon')]
+])
+if Config.START_PIC:
+await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
+else:
+await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
