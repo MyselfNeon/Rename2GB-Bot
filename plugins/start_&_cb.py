0dec5d4a4e1c0266eb7edaed1e4dd3755cebfc42
NeonAn <justasneon@gmail.com>
2025-09-16 00:10:41 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -6,19 +6,19 @@ from config import Config, Txt
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
-user = message.from_user
-await jishubotz.add_user(client, message)
-button = InlineKeyboardMarkup([
-[InlineKeyboardButton('🔊 Uᴘᴅᴀᴛᴇs', url='https://t.me/NeonFiles'),
-InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/+o1s-8MppL2syYTI9')],
-[InlineKeyboardButton('❤️‍🩹 Aʙᴏᴜᴛ', callback_data='about'),
-InlineKeyboardButton('🛠️ Hᴇʟᴘ', callback_data='help')],
-[InlineKeyboardButton("👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/MyselfNeon')]
-])
-if Config.START_PIC:
-await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)
-else:
-await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+    user = message.from_user
+    await jishubotz.add_user(client, message)                
+    button = InlineKeyboardMarkup([
+        [InlineKeyboardButton('🔊 Updates', url='https://t.me/MadflixBotz'),
+        InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/MadflixBots_Support')],
+        [InlineKeyboardButton('❤️‍🩹 About', callback_data='about'),
+        InlineKeyboardButton('🛠️ Help', callback_data='help')],
+        [InlineKeyboardButton("👨‍💻 Developer 🧑‍💻", url='https://t.me/MadflixSupport')]
+    ])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button, quote=True)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True, quote=True)
 
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
