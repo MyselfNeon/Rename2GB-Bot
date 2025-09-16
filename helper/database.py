7ea3a19e40d34e2eae65ab1df1d6b6f499ac2fdb
NeonAn <justasneon@gmail.com>
2025-09-17 00:56:49 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,6 +2,7 @@ import motor.motor_asyncio
 from config import Config
 from .utils import send_log
 
+
 class Database:
 
     def __init__(self, uri, database_name):
@@ -11,20 +12,20 @@ class Database:
 
     def new_user(self, id):
         return dict(
-            _id=int(id),                                   
-            file_id = None,
-            caption = None,
-            prefix = None,
-            suffix = None,
-            metadata = False,
-            metadata_code = "By :- @Madflix_Bots"
+            _id=int(id),
+            file_id=None,
+            caption=None,
+            prefix=None,
+            suffix=None,
+            metadata=False,
+            metadata_code="By :- @Madflix_Bots"
         )
 
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
             user = self.new_user(u.id)
-            await self.col.insert_one(user)            
+            await self.col.insert_one(user)
             await send_log(b, u)
 
     async def is_user_exist(self, id):
@@ -36,83 +37,83 @@ class Database:
         return count
 
     async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+        return self.col.find({})
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'_id': int(user_id)})
-    
 
-
-    #======================= Thumbnail ========================#
+    # ======================= Thumbnail ======================= #
 
     async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'file_id': file_id}}
+        )
 
     async def get_thumbnail(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('file_id', None)
-    
-    
 
-    #======================= Caption ========================#
+    # ======================= Caption ======================= #
 
     async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'caption': caption}}
+        )
 
     async def get_caption(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('caption', None)
 
-
-
-    #======================= Prefix ========================#
+    # ======================= Prefix ======================= #
 
     async def set_prefix(self, id, prefix):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})  
-        
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'prefix': prefix}}
+        )
+
     async def get_prefix(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('prefix', None)      
-    
+        return user.get('prefix', None)
 
-
-    #======================= Suffix ========================#
+    # ======================= Suffix ======================= #
 
     async def set_suffix(self, id, suffix):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})  
-        
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'suffix': suffix}}
+        )
+
     async def get_suffix(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('suffix', None)
 
+    # ======================= Metadata ======================= #
 
-
-    #======================= Metadata ========================#
-        
     async def set_metadata(self, id, bool_meta):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
-        
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'metadata': bool_meta}}
+        )
+
     async def get_metadata(self, id):
         user = await self.col.find_one({'_id': int(id)})
         return user.get('metadata', None)
-        
-        
-        
-    #======================= Metadata Code ========================#    
-        
+
+    # ======================= Metadata Code ======================= #
+
     async def set_metadata_code(self, id, metadata_code):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
+        await self.col.update_one(
+            {'_id': int(id)},
+            {'$set': {'metadata_code': metadata_code}}
+        )
 
     async def get_metadata_code(self, id):
         user = await self.col.find_one({'_id': int(id)})
-        return user.get('metadata_code', None)   
+        return user.get('metadata_code', None)
 
 
-
+# Global DB instance
 jishubotz = Database(Config.DB_URL, Config.DB_NAME)
-
-
-
-
-
