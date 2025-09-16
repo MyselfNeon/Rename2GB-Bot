c6e76fde0ab45bec6a8489a216a1c663a7a87b30
NeonAn <justasneon@gmail.com>
2025-09-17 01:21:19 +0530

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -1,28 +1,43 @@
-from pyrogram import Client, filters 
+from pyrogram import Client, filters
 from helper.database import jishubotz
 
 
+# ======================= VIEW THUMB ======================= #
+
 @Client.on_message(filters.private & filters.command(['view_thumb', 'viewthumb']))
-async def viewthumb(client, message):    
+async def view_thumb(client, message):
+    """Send the user's saved thumbnail, if exists."""
     thumb = await jishubotz.get_thumbnail(message.from_user.id)
+
     if thumb:
-       await client.send_photo(chat_id=message.chat.id, photo=thumb)
+        await client.send_photo(chat_id=message.chat.id, photo=thumb)
     else:
-        await message.reply_text("<b><i>Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴʏ Tʜᴜᴍʙɴᴀɪʟ ❌</i></b>") 
-		
+        await message.reply_text(
+            "<b><i>Yᴏᴜ Dᴏɴ'ᴛ Hᴀᴠᴇ Aɴʏ Tʜᴜᴍʙɴᴀɪʟ ❌</i></b>"
+        )
+
+
+# ======================= DELETE THUMB ======================= #
+
 @Client.on_message(filters.private & filters.command(['del_thumb', 'delthumb']))
-async def removethumb(client, message):
+async def remove_thumb(client, message):
+    """Delete the user's saved thumbnail."""
     await jishubotz.set_thumbnail(message.from_user.id, file_id=None)
-    await message.reply_text("<b><i>Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ 🗑️</i></b>")
-	
+    await message.reply_text(
+        "<b><i>Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ 🗑️</i></b>"
+    )
+
+
+# ======================= SAVE THUMB ======================= #
+
 @Client.on_message(filters.private & filters.photo)
-async def addthumbs(client, message):
-    mkn = await message.reply_text("<b><i>Pʟᴇᴀsᴇ Wᴀɪᴛ ...</i></b>")
-    await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
-    await mkn.edit("<i><b>Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅️</b></i>")
+async def add_thumb(client, message):
+    """Save a new thumbnail from user's sent photo."""
+    temp = await message.reply_text("<b><i>Pʟᴇᴀsᴇ Wᴀɪᴛ ...</i></b>")
+    await jishubotz.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)
+    await temp.edit("<b><i>Tʜᴜᴍʙɴᴀɪʟ Sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ ✅️</i></b>")
 
 
-
-
-
-
+# Dont remove Credits
+# Developer Telegram @MyselfNeon
+# Update channel - @NeonFiles
