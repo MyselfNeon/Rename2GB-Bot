f968005fe9459d24006cb88d63f5f6fd07ddf10c
NeonAn <justasneon@gmail.com>
2025-09-20 20:49:52 +0530

    Update route.py

--- route.py
+++ route.py
@@ -6,7 +6,7 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
     return web.Response(
         text="""
-        <body style="background-color:black; color:yellow; display:flex; justify-content:center; align-items:flex-start; height:100vh; margin:0; font-family:sans-serif; padding-top:20vh; font-size:4rem; text-shadow: 0 0 5px yellow, 0 0 10px yellow, 0 0 20px yellow, 0 0 40px yellow;">
+        <body style="background-color:black; color:yellow; display:flex; justify-content:center; align-items:flex-start; height:100vh; margin:0; font-family:sans-serif; padding-top:20vh; font-size:4rem;">
             Coded By @MyselfNeon
         </body>
         """,
@@ -17,3 +17,4 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+    
